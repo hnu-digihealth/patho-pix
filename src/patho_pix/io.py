8d72566d87fb3376af876d9b8fb9fa9492eb4b9e
muellerdo <domey038@live.de>
2024-05-30 14:16:26 +0200

    feat: added IO for WSI and mask (histolab)

--- src/patho_pix/io.py
+++ src/patho_pix/io.py
@@ -6,17 +6,32 @@
 #                   Library imports                   #
 #-----------------------------------------------------#
 from histolab.slide import Slide
+import tempfile
 
 #-----------------------------------------------------#
 #                    Image Loader                     #
 #-----------------------------------------------------#
-def load_wsi(path):
-    pass
-
-
+def load_wsi(path, use_largeimage=True):
+    # Create temporary directory for WSI scan tiles later
+    pathdir_tiles_img = tempfile.TemporaryDirectory(prefix="patho-pix.tmp.",
+                                                    suffix=".tiles_img")
+    # Load WSI scan via histolab
+    wsi_slide = Slide(path=path, 
+                      processed_path=pathdir_tiles_img.name,
+                      use_largeimage=use_largeimage)
+    # Return histolab WSI
+    return wsi_slide, pathdir_tiles_img
 
 #-----------------------------------------------------#
 #                     Mask Loader                     #
 #-----------------------------------------------------#
-def load_mask(path):
-    pass
+def load_mask(path, use_largeimage=True):
+    # Create temporary directory for WSI mask tiles later
+    pathdir_tiles_mask = tempfile.TemporaryDirectory(prefix="patho-pix.tmp.",
+                                                     suffix=".tiles_mask")
+    # Load WSI mask via histolab
+    wsi_slide = Slide(path=path, 
+                      processed_path=pathdir_tiles_mask.name,
+                      use_largeimage=use_largeimage)
+    # Return histolab WSI
+    return wsi_slide, pathdir_tiles_mask
