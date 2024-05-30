7d24ca7de904738f6fbfa12d1a0afcc312ab8166
Moritz Dinser <moritz.dinser@hnu.de>
2024-05-30 16:46:51 +0000

    chore: make linter happy :)

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -5,16 +5,13 @@ import os
 import requests
 from patho_pix.normalization import normalize_tile_reinhard, normalize_tile_macenko
 
-
-test_img_url= 'https://user-images.githubusercontent.com/31658006/212924179-a85573b6-1bb3-4f9b-a8ab-00a26b1d652e.png'
+test_img_url = 'https://user-images.githubusercontent.com/31658006/212924179-a85573b6-1bb3-4f9b-a8ab-00a26b1d652e.png'
 
 
 class NormalizationTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-
         self.test_img = None
-
         # Create temporary directory for dummy data
         tmp_data = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
         # Load test image
@@ -23,9 +20,8 @@ class NormalizationTests(unittest.TestCase):
             path_img = os.path.join(tmp_data.name, "test.jpg")
             with open(path_img, "wb") as fd:
                 fd.write(response.content)
-                self.test_img=Image.open(path_img)
+                self.test_img = Image.open(path_img)
                 self.test_img.save("/workspaces/patho-pix/output/test_out/test_img.tiff")
-
 
     def test_normalize_tile_reinhard(self):
         # Test Reinhard stain normalization
@@ -34,11 +30,10 @@ class NormalizationTests(unittest.TestCase):
         self.assertIsInstance(self.normalized_tile, Image.Image)
         # Save image in output/test_out for inspection
         self.normalized_tile.save("/workspaces/patho-pix/output/test_out/normalized_tile_reinhard.tiff")
-        
 
     def test_normalize_tile_macenko(self):
         # Test Macenko stain normalization
-        self.normalized_tile= normalize_tile_macenko(self.test_img)
+        self.normalized_tile = normalize_tile_macenko(self.test_img)
         self.assertIsInstance(self.normalized_tile, Image.Image)
         # Save image in output/test_out for inspection
         self.normalized_tile.save("/workspaces/patho-pix/output/test_out/normalized_tile_macenko.tiff")
