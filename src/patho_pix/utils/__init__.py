019cf21b3293033e47ea5d7a1a46e95b92d68af6
muellerdo <domey038@live.de>
2024-05-30 23:13:18 +0200

    feat: added custom GridTiler - AweseomeTiler

--- src/patho_pix/utils/__init__.py
+++ src/patho_pix/utils/__init__.py
@@ -1,6 +1,7 @@
 from .jpeg_to_tiff import convert_jpeg_to_tiff
-
+from .custom_tiler import AwesomeTiler
 
 __all__ = [
-    'convert_jpeg_to_tiff'
+    'convert_jpeg_to_tiff',
+    'AwesomeTiler'
 ]
