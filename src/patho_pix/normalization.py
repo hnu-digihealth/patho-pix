de3357992f0c91272de4dff3eab64cef7942400c
Moritz Dinser <moritz.dinser@hnu.de>
2024-05-30 16:37:05 +0000

    feat: finished normalization.py, added test_normalization.py

--- src/patho_pix/normalization.py
+++ src/patho_pix/normalization.py
@@ -5,12 +5,14 @@ from PIL import Image
 
 from histolab.stain_normalizer import ReinhardStainNormalizer, MacenkoStainNormalizer
 
-
+# D Download link for target image
 img_url = "https://user-images.githubusercontent.com/31658006/212924301-c80f454e-f99a-4479-9852-6ef988c078aa.png"
-test_url= 'https://user-images.githubusercontent.com/31658006/212924179-a85573b6-1bb3-4f9b-a8ab-00a26b1d652e.png'
+
 target_img = None
-test_img = None
+
+# Create a temporary directory to store the target image
 tmp_data = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
+
 # Download Target image
 response = requests.get(img_url)
 if response.status_code == 200:
@@ -18,30 +20,20 @@ if response.status_code == 200:
     with open(path_img, "wb") as fd:
         fd.write(response.content)
         target_img = Image.open(path_img)
-        target_img.save("/workspaces/patho-pix/output/test_out/target.tiff")
+        target_img.save("/workspaces/patho-pix/output/test_out/target_img.tiff")
 else:
-    print("Target Image could not be downloaded")
+    print("Target Image could not be downloaded, please check if the link is valid.")
 
-response = requests.get(test_url)
-if response.status_code == 200:
-    path_img = os.path.join(tmp_data.name, "test.jpg")
-    with open(path_img, "wb") as fd:
-        fd.write(response.content)
-        test_img=Image.open(path_img)
-        test_img.save("/workspaces/patho-pix/output/test_out/test.tiff")
 
-else: print('Test Image could not be downloaded')
-
-def normalize_tile_reinhard(tile, dict):
+def normalize_tile_reinhard(tile):
     normalizer = ReinhardStainNormalizer()
     normalizer.fit(target_img)
-    normalized_img = normalizer.transform(test_img)
-    normalized_img.save("/workspaces/patho-pix/output/test_out/normalized.tiff")
-    return normalized_img, dict
+    normalized_img = normalizer.transform(tile)
+    return normalized_img
 
 
-def normalize_tile_macenko(tile, dict):
+def normalize_tile_macenko(tile):
     normalizer = MacenkoStainNormalizer()
     normalizer.fit(target_img)
     normalized_img = normalizer.transform(tile)
-    return normalized_img, dict
+    return normalized_img
---
+++ tests/test_normalization.py
@@ -0,0 +1,44 @@
+import tempfile
+import unittest
+from PIL import Image
+import os
+import requests
+from patho_pix.normalization import normalize_tile_reinhard, normalize_tile_macenko
+
+
+test_img_url= 'https://user-images.githubusercontent.com/31658006/212924179-a85573b6-1bb3-4f9b-a8ab-00a26b1d652e.png'
+
+
+class NormalizationTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+
+        self.test_img = None
+
+        # Create temporary directory for dummy data
+        tmp_data = tempfile.TemporaryDirectory(prefix="tmp.patho-pix.")
+        # Load test image
+        response = requests.get(test_img_url)
+        if response.status_code == 200:
+            path_img = os.path.join(tmp_data.name, "test.jpg")
+            with open(path_img, "wb") as fd:
+                fd.write(response.content)
+                self.test_img=Image.open(path_img)
+                self.test_img.save("/workspaces/patho-pix/output/test_out/test_img.tiff")
+
+
+    def test_normalize_tile_reinhard(self):
+        # Test Reinhard stain normalization
+        self.normalized_tile = normalize_tile_reinhard(self.test_img)
+        print(type(self.normalized_tile))
+        self.assertIsInstance(self.normalized_tile, Image.Image)
+        # Save image in output/test_out for inspection
+        self.normalized_tile.save("/workspaces/patho-pix/output/test_out/normalized_tile_reinhard.tiff")
+        
+
+    def test_normalize_tile_macenko(self):
+        # Test Macenko stain normalization
+        self.normalized_tile= normalize_tile_macenko(self.test_img)
+        self.assertIsInstance(self.normalized_tile, Image.Image)
+        # Save image in output/test_out for inspection
+        self.normalized_tile.save("/workspaces/patho-pix/output/test_out/normalized_tile_macenko.tiff")
