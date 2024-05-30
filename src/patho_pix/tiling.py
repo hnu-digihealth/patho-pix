df565f70f60b01cf8d86a925e02393ac9db54101
muellerdo <domey038@live.de>
2024-05-30 17:53:31 +0200

    feat: first mask and tiling approach

---
+++ src/patho_pix/tiling.py
@@ -0,0 +1,25 @@
+# =========================================================== #
+#  Author:       Hackathon UKSH Team 14 - Patho-Pix           #
+#  Copyright:    Patho-Pix Team - 2024                        #
+# =========================================================== #
+# ---------------------------------------------------- #
+#                   Library imports                    #
+# ---------------------------------------------------- #
+from histolab.tiler import GridTiler
+from histolab.masks import TissueMask
+
+# ---------------------------------------------------- #
+#           Apply Tiling base on Tissue Mask           #
+# ---------------------------------------------------- #
+def tile_wsi(wsi, tile_size=(1024, 1024)):
+    # Initialize Tiler
+    wsi_tiler = GridTiler(tile_size=tile_size,
+                            check_tissue=True,
+                            tissue_percent=80.0,
+                            prefix="patho-fix.",
+                            suffix=".png"
+                            )
+    # extract tile
+    wsi_tiler.extract(wsi,
+                      #extraction_mask=TissueMask()
+                      )
