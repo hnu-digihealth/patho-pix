fba6d7cf28599ee4297189c7eb1fd478a2824b81
muellerdo <domey038@live.de>
2024-05-30 14:59:49 +0200

    style: enhanced coding style to requirements

--- src/patho_pix/io.py
+++ src/patho_pix/io.py
@@ -1,37 +1,48 @@
-#==============================================================================#
-#  Author:       Hackathon UKSH Team 14 - Patho-Pix                            #
-#  Copyright:    Patho-Pix Team - 2024                                         #
-#==============================================================================#
-#-----------------------------------------------------#
-#                   Library imports                   #
-#-----------------------------------------------------#
-from histolab.slide import Slide
+# =========================================================== #
+#  Author:       Hackathon UKSH Team 14 - Patho-Pix           #
+#  Copyright:    Patho-Pix Team - 2024                        #
+# =========================================================== #
+# -----------------------------------------------------#
+#                   Library imports                    #
+# -----------------------------------------------------#
+# Standard Library
 import tempfile
 
-#-----------------------------------------------------#
-#                    Image Loader                     #
-#-----------------------------------------------------#
+# Third Party
+from histolab.slide import Slide
+
+
+# -----------------------------------------------------#
+#                    Image Loader                      #
+# -----------------------------------------------------#
 def load_wsi(path, use_largeimage=True):
     # Create temporary directory for WSI scan tiles later
-    pathdir_tiles_img = tempfile.TemporaryDirectory(prefix="patho-pix.tmp.",
-                                                    suffix=".tiles_img")
+    pathdir_tiles_img = tempfile.TemporaryDirectory(
+        prefix="patho-pix.tmp.", suffix=".tiles_img"
+    )
     # Load WSI scan via histolab
-    wsi_slide = Slide(path=path, 
-                      processed_path=pathdir_tiles_img.name,
-                      use_largeimage=use_largeimage)
+    wsi_slide = Slide(
+        path=path, 
+        processed_path=pathdir_tiles_img.name, 
+        use_largeimage=use_largeimage
+    )
     # Return histolab WSI
     return wsi_slide, pathdir_tiles_img
 
-#-----------------------------------------------------#
-#                     Mask Loader                     #
-#-----------------------------------------------------#
+
+# -----------------------------------------------------#
+#                     Mask Loader                      #
+# -----------------------------------------------------#
 def load_mask(path, use_largeimage=True):
     # Create temporary directory for WSI mask tiles later
-    pathdir_tiles_mask = tempfile.TemporaryDirectory(prefix="patho-pix.tmp.",
-                                                     suffix=".tiles_mask")
+    pathdir_tiles_mask = tempfile.TemporaryDirectory(
+        prefix="patho-pix.tmp.", suffix=".tiles_mask"
+    )
     # Load WSI mask via histolab
-    wsi_slide = Slide(path=path, 
-                      processed_path=pathdir_tiles_mask.name,
-                      use_largeimage=use_largeimage)
+    wsi_slide = Slide(
+        path=path, 
+        processed_path=pathdir_tiles_mask.name, 
+        use_largeimage=use_largeimage
+    )
     # Return histolab WSI
     return wsi_slide, pathdir_tiles_mask
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,25 +1,32 @@
-#==============================================================================#
-#  Author:       Hackathon UKSH Team 14 - Patho-Pix                            #
-#  Copyright:    Patho-Pix Team - 2024                                         #
-#==============================================================================#
-#-----------------------------------------------------#
-#                   Library imports                   #
-#-----------------------------------------------------#
-#External libraries
-import unittest
+# =========================================================== #
+#  Author:       Hackathon UKSH Team 14 - Patho-Pix           #
+#  Copyright:    Patho-Pix Team - 2024                        #
+# =========================================================== #
+# -----------------------------------------------------#
+#                   Library imports                    #
+# -----------------------------------------------------#
+# External libraries
+# Standard Library
 import os
+import tempfile
+import unittest
+
+# Third Party
 import numpy as np
 import requests
-import tempfile
-# Internal libraries
-from patho_pix.io import load_wsi, load_mask
 
-#-----------------------------------------------------#
-#                    Configuration                    #
-#-----------------------------------------------------#
+# patho_pix
+# Internal libraries
+from patho_pix.io import load_mask, load_wsi
+
+# ---------------------------------------------------- #
+#                    Configuration                     #
+# ---------------------------------------------------- #
 # Download links
-url_img = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?path=/external/gbm/prod16/0534336905/0534336905_boundary.aff&mime=1&fileout=266289986_2.jpg&zoom=9&top=22304&left=25024&width=15040&height=18080"
-url_mask = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?path=/external/gbm/prod16/0534336761/0534336761_annotation.aff&mime=1&fileout=265854792_1.mask.jpg&zoom=9&top=23424&left=18112&width=15104&height=18176"
+url_img = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?" + \
+          "path=/external/gbm/prod16/0534336905/0534336905_boundary.aff&mime=1&fileout=266289986_2.jpg&zoom=9&top=22304&left=25024&width=15040&height=18080"
+url_mask = "http://glioblastoma.alleninstitute.org/cgi-bin/imageservice?" + \
+          "path=/external/gbm/prod16/0534336761/0534336761_annotation.aff&mime=1&fileout=265854792_1.mask.jpg&zoom=9&top=23424&left=18112&width=15104&height=18176"
 path_img = None
 path_mask = None
 
@@ -27,9 +34,10 @@ path_mask = None
 # debug_path_img = "../data/dummy_wsi_01.image.jpg"
 # debug_path_mask = "../data/dummy_wsi_01.mask.jpg"
 
-#-----------------------------------------------------#
-#          Unittest: Input/Output Interface           #
-#-----------------------------------------------------#
+
+# ---------------------------------------------------- #
+#          Unittest: Input/Output Interface            #
+# ---------------------------------------------------- #
 class IOTEST(unittest.TestCase):
     # Download dummy data
     @classmethod
@@ -50,19 +58,19 @@ class IOTEST(unittest.TestCase):
             self.path_mask = os.path.join(self.tmp_data.name, "mask.jpg")
             with open(self.path_mask, "wb") as fd:
                 fd.write(response.content)
-                
-    #-------------------------------------------------#
+
+    # -------------------------------------------------#
     #               Test: Image Loading               #
-    #-------------------------------------------------#
+    # -------------------------------------------------#
     def test_load_image(self):
         print(os.path.exists(self.path_img))
         wsi, path_tiles_wsi = load_wsi(self.path_img)
         self.assertTrue(np.array_equal(wsi.level_dimensions(level=0), 
                                       (15040, 18080)))
 
-    #-------------------------------------------------#
+    # -------------------------------------------------#
     #                Test: Mask Loading               #
-    #-------------------------------------------------#
+    # -------------------------------------------------#
     def test_load_mask(self):
         print(os.path.exists(self.path_mask))
         mask, path_tiles_mask = load_mask(self.path_mask)
